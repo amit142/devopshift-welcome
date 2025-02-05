156c49cb238b08e6cde101a6aa2b89971626b1a1
Your Name <you@example.com>
2025-02-05 14:13:19 +0000

    lab103

--- labs/lab103/main.tf
+++ labs/lab103/main.tf
@@ -1,66 +1,17 @@
-provider "aws" {
-  region = var.region
-}
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true  # Get the latest AMI
 
-variable "region" {
-  default = "us-east-1"
-}
-
-resource "aws_security_group" "sg" {
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]  # Amazon Linux 2
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  filter {
+    name   = "owner-id"
+    values = ["137112412989"]  # Amazon's official AWS account ID
   }
 }
 
-resource "aws_instance" "vm" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
-
-  vpc_security_group_ids = [aws_security_group.sg.id]
-
-  tags = {
-    Name = "amit-vm"
-  }
-}
-
-output "vm_public_ip" {
-  value      = aws_instance.vm.public_ip
-  depends_on = [null_resource.check_public_ip]
-  description = "Public IP address of the VM"
-}
-resource "null_resource" "run_script" {
-  provisioner "local-exec" {
-    command = "echo 'Running a script after provisioning.'"
-  }
-}
-resource "null_resource" "print_hello" {
-  provisioner "local-exec" {
-    command = "echo 'Hello, World!'"
-  }
-}
-variable "emptyip" {
-  default = ""
-}
-
-resource "null_resource" "check_public_ip" {
-  provisioner "local-exec" {
-    command = <<EOT
-      if [ -z "${aws_instance.vm.public_ip}" ]; then
-        echo "ERROR: Public IP address was not assigned." >&2
-        exit 1
-      fi
-    EOT
-  }
-
-  depends_on = [aws_instance.vm]  
+output "ami_id" {
+  value = data.aws_ami.latest_amazon_linux.id
 }
---
+++ labs/lab103/network.tf
@@ -0,0 +1,29 @@
+
+resource "aws_security_group" "sg" {
+  name        = "allow_ssh_http"
+  description = "Security group allowing SSH and HTTP access"
+
+  # Allow SSH (port 22) from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTP (port 80) from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
